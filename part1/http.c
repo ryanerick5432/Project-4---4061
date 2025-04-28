6b68e697e657021c2b217b339e78b02f03d824d4
root <lyon0213@umn.edu>
2025-04-28 19:02:02 +0000

    lots of work done

--- part1/http.c
+++ part1/http.c
@@ -30,10 +30,54 @@ const char *get_mime_type(const char *file_extension) {
 
 int read_http_request(int fd, char *resource_name) {
     // TODO Not yet implemented
+    char buf[BUFSIZE];
+    int bytes_read;
+    int checked = 0;
+    while ((bytes_read = read(fd, buf, BUFSIZE)) > 0) {
+        if (checked == 0) {
+            char *portion = strtok(buf, " ");
+            portion = strtok(NULL, " ");
+            strcpy(resource_name, portion);
+            checked++;
+        }
+    }
+    if (bytes_read < 0) {
+        perror("read");
+        return -1;
+    }
     return 0;
 }
 
 int write_http_response(int fd, const char *resource_path) {
     // TODO Not yet implemented
+    char buf[BUFSIZE];
+    struct stat *stat_buf;
+    if (stat(resource_path, stat_buf) == -1) {
+        if (errno != ENOENT) {
+            perror("stat");
+            return -1;
+        } else {
+            strcpy(buf, "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+
+            if (write(fd, buf, strlen(buf) + 1) == -1) {
+                perror("write");
+                return 1;
+            }
+        }
+    }
+
+    int content_length = stat_buf->st_size;
+    char *file_cpy;
+    strcpy(file_cpy, resource_path);
+    if (strtok(file_cpy, ".") == -1) {
+        perror("strtok");
+        return -1;
+    }
+    char *extens = ".";
+    strcat(extens, file_cpy);
+    char *content_type = get_mime_type(extens);
+
+    strcpy(buf, "HTTP/1.0 200 OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n");
+
     return 0;
 }
--- part1/http_server.c
+++ part1/http_server.c
@@ -22,15 +22,97 @@ void handle_sigint(int signo) {
 }
 
 int main(int argc, char **argv) {
+    struct sigaction sa;
+    sa.sa_handler = handle_sigint;
+
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        perror("sigaction");
+        return 1;
+    }
+
     // First argument is directory to serve, second is port
     if (argc != 3) {
         printf("Usage: %s <directory> <port>\n", argv[0]);
         return 1;
     }
     // Uncomment the lines below to use these definitions:
-    // const char *serve_dir = argv[1];
-    // const char *port = argv[2];
+    const char *serve_dir = argv[1];
+    const char *port = argv[2];
 
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    struct addrinfo *server;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_INET;
+
+    if (getaddrinfo(serve_dir, port, &hints, &server) == -1) {
+        perror("getaddrinfo");
+        return 1;
+    }
+
+    int sockfd;
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        freeaddrinfo(&hints);
+        return 1;
+    }
+
+    if (bind(sockfd, server->ai_addr, server->ai_addrlen) == -1) {
+        perror("bind");
+        close(sockfd);
+        freeaddrinfo(&hints);
+        return 1;
+    }
+
+    freeaddrinfo(server);
+
+    if (listen(sockfd, 0) == -1) {
+        perror("listen");
+        close(sockfd);
+        return 1;
+    }
+
+    while (keep_going) {
+        int client_fd = accept(sockfd, NULL, NULL);
+        if (client_fd == -1) {
+            if (errno != EINTR) {
+                perror("accept");
+                close(sockfd);
+                return 1;
+            } else {
+                break;
+            }
+        }
+
+        char temp[BUFSIZE];
+        if (read_http_request(client_fd, temp) == -1) {
+            fprintf(stderr, "read_http_request");
+            close(client_fd);
+            close(sockfd);
+            return 1;
+        }
+        char path_var[BUFSIZE];
+        strcpy(path_var, serve_dir);
+        strcat(path_var, temp);
+
+        if (write_http_response(client_fd, path_var) == -1) {
+            fprintf(stderr, "write_http_request");
+            close(client_fd);
+            close(sockfd);
+            return 1;
+        }
+
+        if (close(client_fd) == -1) {
+            perror("close");
+            close(sockfd);
+            return 1;
+        }
+    }
+
+    if (close(sockfd) == -1) {
+        perror("close");
+        return 1;
+    }
     // TODO Complete the rest of this function
     return 0;
 }
