5678eedb69c3ec698d780dbd6c9ceb6ccd5188e4
Ryan Erickson <ryanerick5432@gmail.com>
2025-04-30 22:33:10 +0000

    initial work on part2 http_server.c

--- part2/http_server.c
+++ part2/http_server.c
@@ -5,6 +5,7 @@
 #include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
@@ -25,13 +26,48 @@ void handle_sigint(int signo) {
     keep_going = 0;
 }
 
-int main(int argc, char **argv) {
-    connection_queue_t queue;
-    if (connection_queue_init(&queue) == -1) {
-        fprintf(stderr, "init");
-        return 1;
+void *thread_func(void *arg) {
+    connection_queue_t *queue = (connection_queue_t *) arg;
+    int client_fd = connection_queue_dequeue(queue);
+    if (client_fd == -1) {
+        pthread_exit((void *) 1);
     }
 
+    char temp[BUFSIZE];
+    if (read_http_request(client_fd, temp) == -1) {
+        if (strlen(temp) < 3) {
+            strcpy(temp, "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+
+            if (write(client_fd, temp, strlen(temp)) == -1) {
+                perror("write");
+                pthread_exit((void *) 1);
+            }
+            pthread_exit((void *) 0);
+        } else {
+            fprintf(stderr, "read_http_request");
+            close(client_fd);
+            pthread_exit((void *) 1);
+        }
+    }
+    char path_var[BUFSIZE];
+    strcpy(path_var, serve_dir);
+    strcat(path_var, temp);
+
+    if (write_http_response(client_fd, path_var) == -1) {
+        fprintf(stderr, "write_http_request");
+        close(client_fd);
+        pthread_exit((void *) 1);
+    }
+
+    if (close(client_fd) == -1) {
+        perror("close");
+        pthread_exit((void *) 1);
+    }
+
+    return NULL;
+}
+
+int main(int argc, char **argv) {
     struct sigaction sa;
     sa.sa_handler = handle_sigint;
 
@@ -46,8 +82,14 @@ int main(int argc, char **argv) {
         return 1;
     }
     // Uncomment the lines below to use these definitions:
-    const char *serve_dir = argv[1];
+    serve_dir = argv[1];
     const char *port = argv[2];
+
+    connection_queue_t queue;
+    if (connection_queue_init(&queue) == -1) {
+        fprintf(stderr, "init");
+        return 1;
+    }
 
     struct addrinfo hints;
     memset(&hints, 0, sizeof(struct addrinfo));
@@ -57,18 +99,24 @@ int main(int argc, char **argv) {
 
     if (getaddrinfo(NULL, port, &hints, &server) == -1) {
         perror("getaddrinfo");
+        connection_queue_shutdown(&queue);
+        connection_queue_free(&queue);
         return 1;
     }
 
     int sockfd;
     if ((sockfd = socket(server->ai_family, server->ai_socktype, server->ai_protocol)) == -1) {
         perror("socket");
+        connection_queue_shutdown(&queue);
+        connection_queue_free(&queue);
         freeaddrinfo(&hints);
         return 1;
     }
 
     if (bind(sockfd, server->ai_addr, server->ai_addrlen) == -1) {
         perror("bind");
+        connection_queue_shutdown(&queue);
+        connection_queue_free(&queue);
         close(sockfd);
         freeaddrinfo(&hints);
         return 1;
@@ -78,7 +126,62 @@ int main(int argc, char **argv) {
 
     if (listen(sockfd, LISTEN_QUEUE_LEN) == -1) {
         perror("listen");
+        connection_queue_shutdown(&queue);
+        connection_queue_free(&queue);
         close(sockfd);
+        return 1;
+    }
+
+    sigset_t mask;
+    sigset_t oldm;
+    if (sigfillset(&mask) == -1) {
+        perror("sigemptyset");
+        connection_queue_shutdown(&queue);
+        connection_queue_free(&queue);
+        close(sockfd);
+    }
+    if (sigprocmask(SIG_BLOCK, &mask, &oldm) == -1) {
+        perror("sigprocmask");
+        connection_queue_shutdown(&queue);
+        connection_queue_free(&queue);
+        close(sockfd);
+        return 1;
+    }
+
+    pthread_t *threads = malloc(N_THREADS * sizeof(pthread_t));
+
+    if (threads == NULL) {
+        perror("malloc");
+        connection_queue_shutdown(&queue);
+        connection_queue_free(&queue);
+        close(sockfd);
+        return 1;
+    }
+
+    for (long i = 0; i < N_THREADS; i++) {
+        int result = pthread_create(threads + i, NULL, thread_func, &queue);
+        if (result != 0) {
+            fprintf(stderr, "pthread_create failed: %s\n", strerror(result));
+            close(sockfd);
+            connection_queue_shutdown(&queue);
+            connection_queue_free(&queue);
+            for (int j = 0; j < i; j++) {
+                pthread_join(threads[j], NULL);
+            }
+            free(threads);
+            return 1;
+        }
+    }
+
+    if (sigprocmask(SIG_SETMASK, &oldm, NULL) == -1) {
+        perror("sigprocmask");
+        close(sockfd);
+        connection_queue_shutdown(&queue);
+        connection_queue_free(&queue);
+        for (int j = 0; j < N_THREADS; j++) {
+            pthread_join(threads[j], NULL);
+        }
+        free(threads);
         return 1;
     }
 
@@ -88,51 +191,81 @@ int main(int argc, char **argv) {
             if (errno != EINTR) {
                 perror("accept");
                 close(sockfd);
+                connection_queue_shutdown(&queue);
+                connection_queue_free(&queue);
+                for (int j = 0; j < N_THREADS; j++) {
+                    pthread_join(threads[j], NULL);
+                }
+                free(threads);
                 return 1;
             } else {
                 break;
             }
         }
 
-        char temp[BUFSIZE];
-        if (read_http_request(client_fd, temp) == -1) {
-            if (strlen(temp) < 3) {
-                strcpy(temp, "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\n\r\n");
-
-                if (write(client_fd, temp, strlen(temp)) == -1) {
-                    perror("write");
-                    return 1;
-                }
-                return 0;
-            } else {
-                fprintf(stderr, "read_http_request");
-                close(client_fd);
-                close(sockfd);
-                return 1;
+        if (connection_queue_enqueue(&queue, client_fd) == -1) {
+            perror("connection_queue_enqueue");
+            close(sockfd);
+            connection_queue_shutdown(&queue);
+            connection_queue_free(&queue);
+            for (int j = 0; j < N_THREADS; j++) {
+                pthread_join(threads[j], NULL);
             }
-        }
-        char path_var[BUFSIZE];
-        strcpy(path_var, serve_dir);
-        strcat(path_var, temp);
-
-        if (write_http_response(client_fd, path_var) == -1) {
-            fprintf(stderr, "write_http_request");
-            close(client_fd);
-            close(sockfd);
+            free(threads);
             return 1;
         }
 
-        if (close(client_fd) == -1) {
-            perror("close");
+        // if (close(client_fd) == -1) {
+        //     perror("close");
+        //     close(sockfd);
+        //     for (int j = 0; j < N_THREADS; j++) {
+        //         pthread_join(threads[j], NULL);
+        //     }
+        //     free(threads);
+        //     return 1;
+        // }
+    }
+
+    if (connection_queue_shutdown(&queue) == -1) {
+        perror("connection_queue_shutdown");
+        close(sockfd);
+        for (int j = 0; j < N_THREADS; j++) {
+            pthread_join(threads[j], NULL);
+        }
+        free(threads);
+        connection_queue_free(&queue);
+        return 1;
+    }
+
+    if (connection_queue_free(&queue) == -1) {
+        perror("connection_queue_free");
+        close(sockfd);
+        for (int j = 0; j < N_THREADS; j++) {
+            pthread_join(threads[j], NULL);
+        }
+        free(threads);
+        return 1;
+    }
+
+    for (int i = 0; i < N_THREADS; i++) {
+        int result = pthread_join(threads[i], NULL);
+        if (result != 0) {
+            fprintf(stderr, "pthread_join failed: %s\n", strerror(result));
+            for (int j = i + 1; j < N_THREADS; j++) {
+                pthread_join(threads[j], NULL);
+            }
+            free(threads);
             close(sockfd);
-            return 1;
         }
     }
+
+    free(threads);
 
     if (close(sockfd) == -1) {
         perror("close");
         return 1;
     }
+
     // TODO Complete the rest of this function
     return 0;
 }
