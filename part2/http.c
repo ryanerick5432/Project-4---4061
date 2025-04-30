9464440193344d5ac8f0128f65b5f200e23a50e7
root <lyon0213@umn.edu>
2025-04-30 20:52:42 +0000

    quick add

--- part2/http.c
+++ part2/http.c
@@ -30,10 +30,99 @@ const char *get_mime_type(const char *file_extension) {
 
 int read_http_request(int fd, char *resource_name) {
     // TODO Not yet implemented
+    char buf[BUFSIZE];
+    int bytes_read;
+    int checked = 0;
+    while ((bytes_read = read(fd, buf, BUFSIZE)) > 0) {
+        if (checked == 0) {
+            char *portion = strtok(buf, " ");
+            portion = strtok(NULL, " ");
+            strcpy(resource_name, portion);
+            checked++;
+        }
+
+        if ((buf[bytes_read - 4] == '\r') && (buf[bytes_read - 1] == '\n')) {
+            break;
+        }
+    }
+    if (bytes_read < 0) {
+        perror("read");
+        return -1;
+    }
+
+    if (strlen(resource_name) < 2) {
+        return -1;
+    }
     return 0;
 }
 
 int write_http_response(int fd, const char *resource_path) {
-    // TODO Not yet implemented
+    char buf[BUFSIZE];
+    memset(buf, 0, BUFSIZE);
+
+    char file_cpy[BUFSIZE];
+    memset(file_cpy, 0, BUFSIZE);
+    strcpy(file_cpy, resource_path);
+
+    char *portion = strtok(file_cpy, ".");
+    portion = strtok(NULL, ".");
+
+    char extens[6];
+    memset(extens, 0, 6);
+    snprintf(extens, 6, ".%s", portion);
+    // const char *content_type = get_mime_type(extens);
+
+    struct stat stat_buf;
+
+    if (stat(resource_path, &stat_buf) == -1) {
+        if (errno != ENOENT) {
+            perror("stat");
+            return -1;
+        } else {
+            strcpy(buf, "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+
+            if (write(fd, buf, strlen(buf)) == -1) {
+                perror("write");
+                return 1;
+            }
+            return 0;
+        }
+    }
+
+    int content_length = stat_buf.st_size;
+
+    snprintf(buf, BUFSIZE, "HTTP/1.0 200 OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n",
+             get_mime_type(extens), content_length);
+
+    if (write(fd, buf, strlen(buf)) == -1) {
+        perror("write");
+        return -1;
+    }
+    // following sourced from simple_http_client.c, from the lecture code.
+    int file_fd = open(resource_path, O_RDONLY, S_IRUSR);
+    if (file_fd == -1) {
+        perror("open");
+        return -1;
+    }
+
+    int bytes_read;
+    while ((bytes_read = read(file_fd, buf, BUFSIZE)) > 0) {
+        if (write(fd, buf, bytes_read) == -1) {
+            perror("write");
+            close(file_fd);
+            return -1;
+        }
+    }
+    if (bytes_read == -1) {
+        perror("read");
+        close(file_fd);
+        return -1;
+    }
+
+    if (close(file_fd) != 0) {
+        perror("close");
+        return -1;
+    }
+
     return 0;
 }
