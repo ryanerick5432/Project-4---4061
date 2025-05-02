0dc1230ff316bb1c51a96c39524736735d0eb065
Ryan Erickson <ryanerick5432@gmail.com>
2025-05-02 17:58:03 +0000

    comments and error checking for part1

--- part1/http.c
+++ part1/http.c
@@ -29,27 +29,30 @@ const char *get_mime_type(const char *file_extension) {
 }
 
 int read_http_request(int fd, char *resource_name) {
-    // TODO Not yet implemented
+    // make a temp var to iterate through the request, reading it to completion
     char buf[BUFSIZE];
     int bytes_read;
     int checked = 0;
     while ((bytes_read = read(fd, buf, BUFSIZE)) > 0) {
+        // if reading the first block (guaranteed to contain the file name requested)
+        // then parse for the file name
         if (checked == 0) {
             char *portion = strtok(buf, " ");
             portion = strtok(NULL, " ");
-            strcpy(resource_name, portion);
+            strncpy(resource_name, portion, BUFSIZE);
             checked++;
         }
-
+        // if at the end of the request, break the loop
         if ((buf[bytes_read - 4] == '\r') && (buf[bytes_read - 1] == '\n')) {
             break;
         }
     }
+    // if ended on an error
     if (bytes_read < 0) {
         perror("read");
         return -1;
     }
-
+    // if the file name is not valid (such as /), error
     if (strlen(resource_name) < 3) {
         return -1;
     }
@@ -57,28 +60,36 @@ int read_http_request(int fd, char *resource_name) {
 }
 
 int write_http_response(int fd, const char *resource_path) {
+    // create the buffer for the http response
     char buf[BUFSIZE];
     memset(buf, 0, BUFSIZE);
 
+    // make a copy of the resource path, so as to not alter it
     char file_cpy[BUFSIZE];
     memset(file_cpy, 0, BUFSIZE);
-    strcpy(file_cpy, resource_path);
+    strncpy(file_cpy, resource_path, BUFSIZE);
 
+    // iterate through the path to find the extension
     char *portion = strtok(file_cpy, ".");
     portion = strtok(NULL, ".");
 
+    // store the extension for the request information
     char extens[6];
     memset(extens, 0, 6);
-    snprintf(extens, 6, ".%s", portion);
+    if (snprintf(extens, 6, ".%s", portion) < 0) {
+        fprintf(stderr, "snprintf");
+        return -1;
+    }
 
     struct stat stat_buf;
 
+    // check that the file exists
     if (stat(resource_path, &stat_buf) == -1) {
-        if (errno != ENOENT) {
+        if (errno != ENOENT) {    // if the error was not due to nonexistent file
             perror("stat");
             return -1;
-        } else {
-            strcpy(buf, "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+        } else {    // if file didnt exist write a 404 not found
+            strncpy(buf, "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\n\r\n", BUFSIZE);
 
             if (write(fd, buf, strlen(buf)) == -1) {
                 perror("write");
@@ -88,23 +99,31 @@ int write_http_response(int fd, const char *resource_path) {
         }
     }
 
+    // find the length of the files contents
     int content_length = stat_buf.st_size;
 
-    snprintf(buf, BUFSIZE, "HTTP/1.0 200 OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n",
-             get_mime_type(extens), content_length);
+    // create the string for the http response
+    if (snprintf(buf, BUFSIZE, "HTTP/1.0 200 OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n",
+                 get_mime_type(extens), content_length) < 0) {
+        fprintf(stderr, "snprintf");
+        return -1;
+    }
 
+    // send the main part of http response
     if (write(fd, buf, strlen(buf)) == -1) {
         perror("write");
         return -1;
     }
 
     // following sourced/based from simple_http_client.c, from the lecture code.
+    // open the file
     int file_fd = open(resource_path, O_RDONLY, S_IRUSR);
     if (file_fd == -1) {
         perror("open");
         return -1;
     }
 
+    // begin to read through the file, and write its contents to the the end of the http response
     int bytes_read;
     while ((bytes_read = read(file_fd, buf, BUFSIZE)) > 0) {
         if (write(fd, buf, bytes_read) == -1) {
@@ -113,12 +132,15 @@ int write_http_response(int fd, const char *resource_path) {
             return -1;
         }
     }
+
+    // if did not read correctly
     if (bytes_read == -1) {
         perror("read");
         close(file_fd);
         return -1;
     }
 
+    // clean up the file
     if (close(file_fd) != 0) {
         perror("close");
         return -1;
--- part1/http_server.c
+++ part1/http_server.c
@@ -22,9 +22,13 @@ void handle_sigint(int signo) {
 }
 
 int main(int argc, char **argv) {
+    // Set up signal handler for interrupt
     struct sigaction sa;
     sa.sa_handler = handle_sigint;
+    // ensure SA_RESTART is not set
+    sa.sa_flags = 0;
 
+    // install the signal mask
     if (sigaction(SIGINT, &sa, NULL) == -1) {
         perror("sigaction");
         return 1;
@@ -39,17 +43,21 @@ int main(int argc, char **argv) {
     const char *serve_dir = argv[1];
     const char *port = argv[2];
 
+    // set up the server
     struct addrinfo hints;
     memset(&hints, 0, sizeof(struct addrinfo));
     struct addrinfo *server;
+    // set up the TCP server flags
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_family = AF_UNSPEC;
 
+    // set up the addrinfo structs to prepare for server
     if (getaddrinfo(NULL, port, &hints, &server) == -1) {
         perror("getaddrinfo");
         return 1;
     }
 
+    // set up the socket to read and write from
     int sockfd;
     if ((sockfd = socket(server->ai_family, server->ai_socktype, server->ai_protocol)) == -1) {
         perror("socket");
@@ -57,6 +65,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    // finalize setting up the socket
     if (bind(sockfd, server->ai_addr, server->ai_addrlen) == -1) {
         perror("bind");
         close(sockfd);
@@ -64,18 +73,22 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    // no longer need the addrinfo struct
     freeaddrinfo(server);
 
+    // mark the socket to act as a server
     if (listen(sockfd, LISTEN_QUEUE_LEN) == -1) {
         perror("listen");
         close(sockfd);
         return 1;
     }
 
+    // while loop to accept server connections - breaks on sigint handler call
     while (keep_going) {
         int client_fd = accept(sockfd, NULL, NULL);
-        if (client_fd == -1) {
-            if (errno != EINTR) {
+        if (client_fd == -1) {       // error check if the client did not connect, etc
+            if (errno != EINTR) {    // if interrupted via expected sigint interruption, cleanup,
+                                     // otherwise error.
                 perror("accept");
                 close(sockfd);
                 return 1;
@@ -84,10 +97,14 @@ int main(int argc, char **argv) {
             }
         }
 
+        // set up the buffer to hold the file name from client tcp request
         char temp[BUFSIZE];
+        // read the clients tcp request and extract the file name
         if (read_http_request(client_fd, temp) == -1) {
+            // if the file name is not valid (as per the construction of this project,
+            // not broadly), prompt for correct output through socket
             if (strlen(temp) < 3) {
-                strcpy(temp, "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+                strncpy(temp, "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\n\r\n", BUFSIZE);
 
                 if (write(client_fd, temp, strlen(temp)) == -1) {
                     perror("write");
@@ -95,17 +112,22 @@ int main(int argc, char **argv) {
                     return 1;
                 }
                 return 0;
-            } else {
+            } else {    // if errored due to another reason
                 fprintf(stderr, "read_http_request");
                 close(client_fd);
                 close(sockfd);
                 return 1;
             }
         }
+        // create a buffer for the path
         char path_var[BUFSIZE];
-        strcpy(path_var, serve_dir);
+        // make a copy of the server directory as per argv - do not change the original argument
+        strncpy(path_var, serve_dir, BUFSIZE);
+        // concatenate the path variable and the extracted file name to use for the response to
+        // client
         strcat(path_var, temp);
 
+        // write the response utilizing the path of the extracted file
         if (write_http_response(client_fd, path_var) == -1) {
             fprintf(stderr, "write_http_request");
             close(client_fd);
@@ -113,6 +135,7 @@ int main(int argc, char **argv) {
             return 1;
         }
 
+        // clean up the current client
         if (close(client_fd) == -1) {
             perror("close");
             close(sockfd);
@@ -120,6 +143,7 @@ int main(int argc, char **argv) {
         }
     }
 
+    // once done, clean up the socket
     if (close(sockfd) == -1) {
         perror("close");
         return 1;
